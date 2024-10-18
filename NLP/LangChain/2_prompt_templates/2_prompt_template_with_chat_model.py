acba601d86cd1666e9688650b573c771414f68fa
benmati ziad <152486493+zyx721@users.noreply.github.com>
2024-10-18 14:30:30 +0100

    finish first and second (chat models and prompt)

---
+++ NLP/LangChain/2_prompt_templates/2_prompt_template_with_chat_model.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+#
+# # PART 1: Create a ChatPromptTemplate using a template string
+# print("-----Prompt from Template-----")
+# template = "Tell me a joke about {topic}."
+# prompt_template = ChatPromptTemplate.from_template(template)
+#
+# prompt = prompt_template.invoke({"topic": "programmers"})
+# result = model.invoke(prompt)
+# print(result.content)
+
+# # PART 2: Prompt with Multiple Placeholders
+# print("\n----- Prompt with Multiple Placeholders -----\n")
+# template_multiple = """You are a helpful assistant.
+# Human: Tell me a {adjective} short story about a {animal}.
+# Assistant:"""
+# prompt_multiple = ChatPromptTemplate.from_template(template_multiple)
+# prompt = prompt_multiple.invoke({"adjective": "funny", "animal": "panda"})
+#
+# result = model.invoke(prompt)
+# print(result.content)
+#
+# PART 3: Prompt with System and Human Messages (Using Tuples)
+print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
+messages = [
+    ("system", "You are a comedian who tells jokes about {topic}."),
+    ("human", "Tell me {joke_count} jokes."),
+]
+prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
+result = model.invoke(prompt)
+print(result.content)
