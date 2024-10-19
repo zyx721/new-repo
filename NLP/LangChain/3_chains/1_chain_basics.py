fef622ac96526d9607fb736f8f5f0fce19defa72
benmati ziad <152486493+zyx721@users.noreply.github.com>
2024-10-19 16:39:32 +0100

    update

---
+++ NLP/LangChain/3_chains/1_chain_basics.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+# model = ChatOpenAI(model="gpt-4o")
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+# Define prompt templates (no need for separate Runnable chains)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a comedian who tells jokes about {topic}."),
+        ("human", "Tell me {joke_count} jokes."),
+    ]
+)
+
+# Create the combined chain using LangChain Expression Language (LCEL)
+chain = prompt_template | model | StrOutputParser()
+# chain = prompt_template | model
+
+# Run the chain
+result = chain.invoke({"topic": "lawyers", "joke_count": 3})
+
+# Output
+print(result)
